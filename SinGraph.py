3eeabbb76d3d71cb876e3dd597930cb93c94b0dc
BenWilkins20 <brjw@hotmail.co.uk>
2017-03-15 14:41:31 +0000

    Updated final report, code for sine wave
    The python file contains a small section of code that generates two sine graphs to demonstrate two behaviour alterations - shrinking and shifting. These graphs are used in the final report.

---
+++ SinGraph.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot1(x, sample):
+    y1 = np.sin(2 * np.pi * 3 * x / sample) + 1
+    y2 = np.sin(2 * np.pi * 3 * x / sample) * 0.5 + 1
+    plt.gca().set_color_cycle(['black', 'red'])
+    p1 = plt.plot(x, y1, label='Original')
+    p2 = plt.plot(x, y2, label='Reduced')
+    plt.legend()
+    plt.xlabel("Time")
+    plt.ylabel('Consumption')
+    plt.show()
+
+def plot2(x, sample):
+    y1 = np.sin((2 * np.pi * 3 * x / sample) - (3*np.pi/4))
+    y2 = np.sin(2 * np.pi * 3 * x / sample)
+    y3 = y1 + y2 + 1
+    plt.gca().set_color_cycle(['red', 'green', 'blue'])
+    p2 = plt.plot(x, y3, label='Combination')
+    p3 = plt.plot(x, y1 + 1, label='Consumer1')
+    p4 = plt.plot(x, y2 + 1, label='Consumer2')
+    plt.legend()
+    plt.xlabel("Time")
+    plt.ylabel('Consumption')
+    plt.show()
+
+sample = 500
+x = np.arange(sample)
+plot1(x, sample)
+plot2(x, sample)
+
+
